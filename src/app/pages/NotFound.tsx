f13246a1d3d716a3dbba366a3a2ccb0749389a73
Kai Dorschner <me@krnl.de>
2021-01-14 13:32:37 +0100

    Add stylelint and pimp eslint

--- src/app/pages/NotFound.tsx
+++ src/app/pages/NotFound.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 
+import { useTranslation } from 'react-i18next';
+
 const NotFound: React.FC = () => {
+  const { t } = useTranslation();
+
   return (
     <div>
-      <h1>404</h1>
-      <p>Page Not Found</p>
+      <h1>{t('NOTFOUND.TITLE')}</h1>
+      <p>{t('NOTFOUND.DESCRIPTION')}</p>
     </div>
   );
 };
